10b6d9ac1cfbbcaa41e5e2be5fe914f00e2b6947
chadoli27 <jasminekim1009@gmail.com>
2024-11-16 14:33:53 +0900

    feat: 채팅

---
+++ src/main/java/edu/example/chat/controller/ChatController.java
@@ -0,0 +1,41 @@
+package edu.example.chat.controller;
+
+import edu.example.chat.model.ChatRoom;
+import edu.example.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+    private final ChatService chatService;
+
+    @RequestMapping("/chat/chatList")
+    public String chatList(Model model){
+        List<ChatRoom> roomList = chatService.findAllRoom();
+        model.addAttribute("roomList", roomList);
+        return "chat/chatList";
+    }
+
+    @PostMapping("/chat/createRoom")
+    public String createRoom(Model model, @RequestParam String name, String username){
+        ChatRoom room = chatService.createRoom(name);
+        model.addAttribute("room", room);
+        model.addAttribute("username", username);
+        return "chat/createRoom";
+    }
+
+    @GetMapping("/chat/chatRoom")
+    public String chatRoom(Model model, @RequestParam String roomId){
+        ChatRoom room = chatService.findRoomById(roomId);
+        model.addAttribute("room", room);
+        return "chat/chatRoom";
+    }
+}
---
+++ src/main/java/edu/example/chat/model/ChatMessage.java
@@ -0,0 +1,19 @@
+package edu.example.chat.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    public enum MessageType{
+        ENTER, TALK, QUIT
+    }
+
+    private MessageType type;
+    private String roomId;
+    private String sender;
+    private String message;
+
+}
